084a2973fbdc9a47af1fcf0a0b591bc817d67c4f
Ryosuke Ikeda <examinare000@gmail.com>
2026-05-10 17:44:54 +0900

    機能: usageルーターをアプリに登録

--- api/main.py
+++ api/main.py
@@ -10,6 +10,7 @@ from .routers.auth import router as auth_router
 from .routers.cards import router as cards_router
 from .routers.products import router as products_router
 from .routers.profile import router as profile_router
+from .routers.usage import router as usage_router
 
 logger = logging.getLogger(__name__)
 
@@ -23,6 +24,7 @@ app.include_router(products_router)
 app.include_router(auth_router)
 app.include_router(cards_router)
 app.include_router(profile_router)
+app.include_router(usage_router)
 
 @app.get("/api/health")
 def health_check():
